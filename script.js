89f627faff55e036ba49302ef8c6b2b2c0ef0a86
zYumoto <victorjuanrazer@gmail.com>
2023-10-29 16:42:33 -0300

    keyboard js

---
+++ script.js
@@ -0,0 +1,32 @@
+var i = 0;
+const palavra = () => {
+    document.getElementById(i).innerHTML = k;
+    i++;
+}
+
+const letraA= () => k = document.getElementById("A").innerHTML; 
+const letraB= () => k = document.getElementById("B").innerHTML; 
+const letraC= () => k = document.getElementById("C").innerHTML; 
+const letraD= () => k = document.getElementById("D").innerHTML; 
+const letraE= () => k = document.getElementById("E").innerHTML; 
+const letraF= () => k = document.getElementById("F").innerHTML; 
+const letraG= () => k = document.getElementById("G").innerHTML; 
+const letraH= () => k = document.getElementById("H").innerHTML; 
+const letraI= () => k = document.getElementById("I").innerHTML; 
+const letraJ= () => k = document.getElementById("J").innerHTML; 
+const letraK= () => k = document.getElementById("K").innerHTML; 
+const letraL= () => k = document.getElementById("L").innerHTML; 
+const letraM= () => k = document.getElementById("M").innerHTML; 
+const letraN= () => k = document.getElementById("N").innerHTML; 
+const letraO= () => k = document.getElementById("O").innerHTML; 
+const letraP= () => k = document.getElementById("P").innerHTML; 
+const letraQ= () => k = document.getElementById("Q").innerHTML; 
+const letraR= () => k = document.getElementById("R").innerHTML; 
+const letraS= () => k = document.getElementById("S").innerHTML; 
+const letraT= () => k = document.getElementById("T").innerHTML; 
+const letraU= () => k = document.getElementById("U").innerHTML; 
+const letraV= () => k = document.getElementById("V").innerHTML; 
+const letraW= () => k = document.getElementById("W").innerHTML; 
+const letraX= () => k = document.getElementById("X").innerHTML; 
+const letraY= () => k = document.getElementById("Y").innerHTML; 
+const letraZ= () => k = document.getElementById("Z").innerHTML; 
