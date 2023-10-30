f8053d8669e2013ba01671b04676bfa1e67538ef
zYumoto <victorjuanrazer@gmail.com>
2023-10-30 20:39:59 -0300

    change js keyboard + words for the game

--- script.js
+++ script.js
@@ -1,32 +1,84 @@
 var i = 0;
-const palavra = () => {
-    document.getElementById(i).innerHTML = k;
-    i++;
+var j;
+var wordArr = [];
+var word;
+const palavras = [
+  "Amarelo",
+  "Amiga",
+  "Amor",
+  "Ave",
+  "Avião",
+  "Avó",
+  "Balão",
+  "Bebê",
+  "Bolo",
+  "Branco",
+  "Cama",
+  "Caneca",
+  "Celular",
+  "Céu",
+  "Clube",
+  "Copo",
+  "Doce",
+  "Elefante",
+  "Escola",
+  "Estojo",
+  "Faca",
+  "Foto",
+  "Garfo",
+  "Geleia",
+  "Girafa",
+  "Janela",
+  "Limonada",
+  "Mãe",
+  "Meia",
+  "Noite",
+  "Óculos",
+  "ônibus",
+  "Ovo",
+  "Pai",
+  "Pão",
+  "Parque",
+  "Passarinho",
+  "Peixe",
+  "Pijama",
+  "Rato"
+];
+
+function sortearPalavra() {
+  const indiceSorteado = Math.floor(Math.random() * palavras.length);
+  return palavras[indiceSorteado];
 }
 
-const letraA= () => k = document.getElementById("A").innerHTML; 
-const letraB= () => k = document.getElementById("B").innerHTML; 
-const letraC= () => k = document.getElementById("C").innerHTML; 
-const letraD= () => k = document.getElementById("D").innerHTML; 
-const letraE= () => k = document.getElementById("E").innerHTML; 
-const letraF= () => k = document.getElementById("F").innerHTML; 
-const letraG= () => k = document.getElementById("G").innerHTML; 
-const letraH= () => k = document.getElementById("H").innerHTML; 
-const letraI= () => k = document.getElementById("I").innerHTML; 
-const letraJ= () => k = document.getElementById("J").innerHTML; 
-const letraK= () => k = document.getElementById("K").innerHTML; 
-const letraL= () => k = document.getElementById("L").innerHTML; 
-const letraM= () => k = document.getElementById("M").innerHTML; 
-const letraN= () => k = document.getElementById("N").innerHTML; 
-const letraO= () => k = document.getElementById("O").innerHTML; 
-const letraP= () => k = document.getElementById("P").innerHTML; 
-const letraQ= () => k = document.getElementById("Q").innerHTML; 
-const letraR= () => k = document.getElementById("R").innerHTML; 
-const letraS= () => k = document.getElementById("S").innerHTML; 
-const letraT= () => k = document.getElementById("T").innerHTML; 
-const letraU= () => k = document.getElementById("U").innerHTML; 
-const letraV= () => k = document.getElementById("V").innerHTML; 
-const letraW= () => k = document.getElementById("W").innerHTML; 
-const letraX= () => k = document.getElementById("X").innerHTML; 
-const letraY= () => k = document.getElementById("Y").innerHTML; 
-const letraZ= () => k = document.getElementById("Z").innerHTML; 
+function iniciarJogo() {
+  word = sortearPalavra();
+  console.log("palavra sorteada: " + word);
+
+  for (let k = 0; k < 10; k++) {
+    document.getElementById(k).innerHTML = "";
+  }
+  wordArr = [];
+  i = 0;
+}
+
+const enter = () => {
+  for (j = 0; j < i; j++) {
+    word += wordArr[j];
+  }
+  console.log(word);
+};
+
+const palavra = () => {
+  word = "";
+  document.getElementById(i).innerHTML = k;
+  wordArr.push(k);
+  i++;
+};
+
+// Funções para o tecladinho funcionar
+const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZÇ".split("");
+for (const letra of letras) {
+  window["letra" + letra] = () => (k = letra);
+}
+
+iniciarJogo();
