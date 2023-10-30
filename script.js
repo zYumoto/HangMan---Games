549ce9a689c7f59da9dd177bcd3e6e26ebbc2b09
zYumoto <victorjuanrazer@gmail.com>
2023-10-30 20:45:30 -0300

    add: type for the words

--- script.js
+++ script.js
@@ -3,46 +3,46 @@ var j;
 var wordArr = [];
 var word;
 const palavras = [
-  "Amarelo",
-  "Amiga",
-  "Amor",
-  "Ave",
-  "Avião",
-  "Avó",
-  "Balão",
-  "Bebê",
-  "Bolo",
-  "Branco",
-  "Cama",
-  "Caneca",
-  "Celular",
-  "Céu",
-  "Clube",
-  "Copo",
-  "Doce",
-  "Elefante",
-  "Escola",
-  "Estojo",
-  "Faca",
-  "Foto",
-  "Garfo",
-  "Geleia",
-  "Girafa",
-  "Janela",
-  "Limonada",
-  "Mãe",
-  "Meia",
-  "Noite",
-  "Óculos",
-  "ônibus",
-  "Ovo",
-  "Pai",
-  "Pão",
-  "Parque",
-  "Passarinho",
-  "Peixe",
-  "Pijama",
-  "Rato"
+  { palavra: "Amarelo", categoria: "Cores" },
+  { palavra: "Amiga", categoria: "Relações" },
+  { palavra: "Amor", categoria: "Sentimentos" },
+  { palavra: "Ave", categoria: "Animais" },
+  { palavra: "Avião", categoria: "Transportes" },
+  { palavra: "Avó", categoria: "Família" },
+  { palavra: "Balão", categoria: "Objetos" },
+  { palavra: "Bebê", categoria: "Família" },
+  { palavra: "Bolo", categoria: "Comida" },
+  { palavra: "Branco", categoria: "Cores" },
+  { palavra: "Cama", categoria: "Móveis" },
+  { palavra: "Caneca", categoria: "Objetos" },
+  { palavra: "Celular", categoria: "Tecnologia" },
+  { palavra: "Céu", categoria: "Natureza" },
+  { palavra: "Clube", categoria: "Entretenimento" },
+  { palavra: "Copo", categoria: "Utensílios" },
+  { palavra: "Doce", categoria: "Comida" },
+  { palavra: "Elefante", categoria: "Animais" },
+  { palavra: "Escola", categoria: "Educação" },
+  { palavra: "Estojo", categoria: "Material Escolar" },
+  { palavra: "Faca", categoria: "Utensílios" },
+  { palavra: "Foto", categoria: "Fotografia" },
+  { palavra: "Garfo", categoria: "Utensílios" },
+  { palavra: "Geleia", categoria: "Comida" },
+  { palavra: "Girafa", categoria: "Animais" },
+  { palavra: "Janela", categoria: "Casa" },
+  { palavra: "Limonada", categoria: "Bebidas" },
+  { palavra: "Mãe", categoria: "Família" },
+  { palavra: "Meia", categoria: "Vestuário" },
+  { palavra: "Noite", categoria: "Tempo" },
+  { palavra: "Óculos", categoria: "Acessórios" },
+  { palavra: "ônibus", categoria: "Transportes" },
+  { palavra: "Ovo", categoria: "Comida" },
+  { palavra: "Pai", categoria: "Família" },
+  { palavra: "Pão", categoria: "Comida" },
+  { palavra: "Parque", categoria: "Lazer" },
+  { palavra: "Passarinho", categoria: "Animais" },
+  { palavra: "Peixe", categoria: "Animais" },
+  { palavra: "Pijama", categoria: "Vestuário" },
+  { palavra: "Rato", categoria: "Animais" }
 ];
 
 function sortearPalavra() {
@@ -51,8 +51,11 @@ function sortearPalavra() {
 }
 
 function iniciarJogo() {
-  word = sortearPalavra();
-  console.log("palavra sorteada: " + word);
+  const palavraSorteada = sortearPalavra();
+  word = palavraSorteada.palavra;
+  const categoria = palavraSorteada.categoria;
+  console.log("Nova palavra sorteada: " + word);
+  console.log("Categoria: " + categoria);
 
   for (let k = 0; k < 10; k++) {
     document.getElementById(k).innerHTML = "";
@@ -73,9 +76,9 @@ const palavra = () => {
   document.getElementById(i).innerHTML = k;
   wordArr.push(k);
   i++;
-};
+}
 
-// Funções para o tecladinho funcionar
+// Funcão Letras
 const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZÇ".split("");
 for (const letra of letras) {
   window["letra" + letra] = () => (k = letra);
